a233c146b321e3a5670b449bbddc03a890532184
quang181 <quangnm1@bizflycloud.vn>
2024-09-28 10:40:28 +0700

    update uri mysql

--- src/comman.py
+++ src/comman.py
@@ -4,7 +4,7 @@ rp_id = "localhost"
 rp_name = "Vcc"
 # Connet mysql
 from sqlalchemy import create_engine
-engine = create_engine('mysql+pymysql://root:1812001q@127.0.0.1:3307/mydb')
+engine = create_engine('mysql+pymysql://root:1812001q@123.30.48.240:3307/mydb')
 
 Session = sessionmaker(bind=engine)
 
